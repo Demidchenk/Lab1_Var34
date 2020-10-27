ee03d332411615490f1d09ab3317eebc8cc511e4
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-10-27 23:56:08 +0200

    Add project files.

---
+++ Lab1_Var34/Lab1_Var34.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a;
+    std::cout << "Ребро = ";
+    std::cin >> a;
+    std::cout << "Об'єм = " << a * a * a << "\nПлоща поверхні = " << a * a * 6;
+}
+
